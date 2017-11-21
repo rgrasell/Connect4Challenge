80fa0b16a98318b43413c8a20d95583a60adf849
Ryan Grasell <rgrasell@apple.com>
2017-11-21 10:16:47 -0800

    Updates to the test and example bots to fit the new player interface.

--- src/main/kotlin/com/grasell/Main.kt
+++ src/main/kotlin/com/grasell/Main.kt
@@ -6,11 +6,11 @@ fun main(args: Array<String>) {
     val board = initializeBoard(4, 4)
     val player1 = object : Player {
         override val name = "player1"
-        override fun takeTurn(board: Connect4Board, turnCallback: (Int) -> Unit) = turnCallback(1)
+        override fun takeTurn(board: Connect4Board, opponent: Player, turnCallback: (Int) -> Unit) = turnCallback(1)
     }
     val player2 = object : Player {
         override val name = "player2"
-        override fun takeTurn(board: Connect4Board, turnCallback: (Int) -> Unit) = turnCallback(2)
+        override fun takeTurn(board: Connect4Board, opponent: Player, turnCallback: (Int) -> Unit) = turnCallback(2)
     }
 
     val winner = simulateGame(player1, player2)
