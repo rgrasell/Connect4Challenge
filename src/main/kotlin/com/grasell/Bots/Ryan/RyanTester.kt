0e4aa4c7112787c7460d1e56d46afa38174a5687
Ryan Grasell <rgrasell@apple.com>
2017-11-23 10:14:40 -0800

    Testing against human player

--- src/main/kotlin/com/grasell/Bots/Ryan/RyanTester.kt
+++ src/main/kotlin/com/grasell/Bots/Ryan/RyanTester.kt
@@ -1,5 +1,6 @@
 package com.grasell.Bots.Ryan
 
+import com.grasell.Bots.Human.HumanPlayer
 import com.grasell.initializeBoard
 import com.grasell.simulateGame
 import kotlin.system.measureTimeMillis
@@ -7,9 +8,8 @@ import kotlin.system.measureTimeMillis
 fun main(args: Array<String>) {
     println("Hello, World")
 
-    val board = initializeBoard(4, 4)
     val player1 = RyanBot()
-    val player2 = RyanBot()
+    val player2 = HumanPlayer()
 
     val time = measureTimeMillis {
         val winner = simulateGame(player1, player2, 10_000)
