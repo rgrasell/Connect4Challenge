7dfbe2ee4262c8b6e8514179729c44933d6c929c
Ryan Grasell <rgrasell@apple.com>
2017-11-23 10:11:42 -0800

    Added a human input player for testing.

---
+++ src/main/kotlin/com/grasell/Bots/Human/humanPlayer.kt
@@ -0,0 +1,20 @@
+package com.grasell.Bots.Human
+
+import com.grasell.Connect4Board
+import com.grasell.Player
+import java.util.*
+
+class HumanPlayer : Player {
+    override fun takeTurn(board: Connect4Board, opponent: Player, turnCallback: (Int) -> Unit) {
+        println("Current board: ")
+        println(board.humanRepresentation(this))
+        print("Enter column number to drop piece: ")
+
+        with(Scanner(System.`in`)) {
+            turnCallback(nextInt())
+        }
+    }
+
+    override val name = "human player"
+
+}
--- src/main/kotlin/com/grasell/Main.kt
+++ src/main/kotlin/com/grasell/Main.kt
@@ -1,20 +1,13 @@
 package com.grasell
 
+import com.grasell.Bots.Human.HumanPlayer
+
 fun main(args: Array<String>) {
     println("Hello, World")
 
     val board = initializeBoard(4, 4)
-    val player1 = object : Player {
-        override val name = "player1"
-        override fun takeTurn(board: Connect4Board, opponent: Player, turnCallback: (Int) -> Unit) = turnCallback(1)
-    }
-    val player2 = object : Player {
-        override val name = "player2"
-        override fun takeTurn(board: Connect4Board, opponent: Player, turnCallback: (Int) -> Unit) {
-            Thread.sleep(100_000)
-            turnCallback(2)
-        }
-    }
+    val player1 = HumanPlayer()
+    val player2 = HumanPlayer()
 
     val winner = simulateGame(player1, player2, 10_000)
 
